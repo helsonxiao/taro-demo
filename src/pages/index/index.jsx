34787009b9528d1155bc6a3152bb157b6fdd7062
helsonxiao <helsonxiao@gmail.com>
2020-12-20 17:43:27 +0800

    init

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
