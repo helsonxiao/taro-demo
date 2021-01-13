6a93a6e0579fd8c7c419c38b841208aef2bd12da
helsonxiao <helsonxiao@gmail.com>
2021-01-13 23:35:14 +0800

    feat: tabbar

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,28 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/user/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: '#fff',
+    selectedColor: '#eb735f',
+    backgroundColor: '#000',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '短视频',
+      },
+      {
+        pagePath: 'pages/user/index',
+        text: '我的',
+      },
+    ],
+  },
 }
---
+++ src/pages/user/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>第二个页面!</Text>
+      </View>
+    )
+  }
+}
